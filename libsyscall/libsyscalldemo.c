3440e2dedbb7b5ebcbfe2617e1b4a9f89b7b3ec7
AbhishekRaj2022 <abhishek052raj@gmail.com>
2022-06-09 07:22:03 +0530

    Library and System call demo

---
+++ libsyscall/libsyscalldemo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+char str[] = "Hello World!";
+
+int main(int argc, char const *argv[])
+{
+    int a;
+    strlen(str);
+    scanf("%d", &a);
+    printf("Hello, World!\n");
+    return 0;
+}
